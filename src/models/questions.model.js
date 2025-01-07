92f535df316489951f22a94827525d162c2a0270
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-07 18:41:16 +0530

    create question model

---
+++ src/models/questions.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  question_id: { type: Number, unique: true, required: true },
+  title: { type: String, required: true },
+  tags: { type: [String], required: true },
+  is_answered: { type: Boolean, required: true },
+  view_count: { type: Number, required: true },
+  answer_count: { type: Number, required: true },
+  score: { type: Number, required: true },
+  closed_date: { type: Date },
+  last_activity_date: { type: Date, required: true },
+  creation_date: { type: Date, required: true },
+  last_edit_date: { type: Date },
+  link: { type: String, required: true },
+  closed_reason: { type: String },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "Owner" },
+});
+
+export const Question = mongoose.model("Question", questionSchema);
+
