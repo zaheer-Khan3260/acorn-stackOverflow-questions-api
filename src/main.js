5698a4a2a11af83edfa0e138e732674cea339ecc
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-07 18:53:31 +0530

    configure database connection ans cors setting

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import dotenv from "dotenv"
+import connectDatabase from "./database/db.js"
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+
+const server = express();
+
+dotenv.config({
+    path: '../env'
+});
+
+console.log("Cors origin env", process.env.CORS_ORIGIN);
+
+const corsOptions = {
+    origin: [process.env.CORS_ORIGIN, "http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+    allowedHeaders: "Content-Type, Authorization, X-Requested-With",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+server.use(cors(corsOptions));
+
+
+server.get("/", (_, res) => {
+    res.send("Hello World from Vite and Express");
+
+})
+
+connectDatabase()
+.then(() => {
+    server.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+})
+
+
+server.use(express.json( {limit: "16kb"}))
+server.use(express.urlencoded({extended: true, limit: "16kb"}));
+server.use(express.static("public"))
+server.use(cookieParser());
+
+
+
+import userRoutes from "./routes/user.routes.js"
+import employeeRouter from "./routes/employee.routes.js"
+
+
+server.use("/api/users", userRoutes)
+server.use("/api/employees", employeeRouter);
