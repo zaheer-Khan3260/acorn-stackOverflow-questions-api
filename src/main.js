4a77792e7cc43bfd135a9c6d34a81d45d4166bae
zaheer-khan3260 <zaheer.khan199835@gmail.com>
2025-01-08 09:43:29 +0530

    add routing to APIs

--- src/main.js
+++ src/main.js
@@ -52,3 +52,6 @@ server.use(express.urlencoded({extended: true, limit: "16kb"}));
 server.use(express.static("public"))
 server.use(cookieParser());
 
+import questionRoutes from "./routes/question.routes.js"
+
+server.use("/api/questions", questionRoutes);
